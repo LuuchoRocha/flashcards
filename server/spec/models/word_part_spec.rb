3397d91de36131d6d80ad35a71cf025c219bfe41
Luciano Rocha <rocha.lucho@gmail.com>
2025-09-27 14:24:11 -0300

    Update WordPart model tests.

--- server/spec/models/word_part_spec.rb
+++ server/spec/models/word_part_spec.rb
@@ -1,7 +1,14 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe WordPart, type: :model do
   it { is_expected.to belong_to(:phonics_level) }
 
   it { is_expected.to validate_presence_of(:label) }
+
+  it do
+    should define_enum_for(:status).
+             with_values(mastered: "mastered", needs_work: "needs_work").
+             backed_by_column_of_type(:string).
+             validating(allowing_nil: true)
+  end
 end
