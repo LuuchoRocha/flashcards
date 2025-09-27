b5efe9e8c6fac22e9900c546b7a25ad84a609005
Luciano Rocha <rocha.lucho@gmail.com>
2025-09-27 14:14:19 -0300

    Add show and update actions to WordPartsController
    The show action will render a specific word part based on its ID,
    and the update action will allow updating a word part's attributes.
    Both actions will respond with JSON format.

--- server/app/controllers/word_parts_controller.rb
+++ server/app/controllers/word_parts_controller.rb
@@ -1,7 +1,31 @@
 class WordPartsController < ApplicationController
+  before_action :set_current_word_part, only: [:show, :update]
+
   def index
     @phonics_level = PhonicsLevel.find(params[:phonics_level_id])
 
     render json: @phonics_level.word_parts
+  end
+
+  def show
+    render json: @word_part
+  end
+
+  def update
+    if @word_part.update(word_part_params)
+      render json: @word_part
+    else
+      render json: { errors: @word_part.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def set_current_word_part
+    @word_part = WordPart.find(params[:id])
+  end
+
+  def word_part_params
+    params.require(:word_part).permit(:status)
   end
 end
